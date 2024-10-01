5a9fd7abd51433dafa82bc062f7d5e4f8a7423cc
Tony Cook <tony@develop-help.com>
2024-10-01 10:52:14 +1000

    add test case for call with macros
    which matches a match found in the perl source.
    
    This match in particular probably shouldn't be a hit, but how do
    I differentiate this from other hits, possibly by both parameters
    being macros, but there's not generic way to say "is this expression
    a macro replacement" with specifying the macro name.
    
    I might need to adapt the isExpandedFromMacro() check to see if we
    find macro replacements that exactly match the expression bounds.
    
    We can't just check if the expression is the result of any macro
    expansion since it's always the result of the sv_setpvn() macro being
    expanded and I don't want to hard code the WARNbits etc macro names
    in the check.

--- foo.c
+++ foo.c
@@ -9,12 +9,16 @@ extern void Perl_sv_setpvn(SV *sv, const char *s, size_t len);
 char buf[10];
 int buflen;
 
+#define WARNbits "1234"
+#define WARNbits_size 4
+
 void foo(SV *sv) {
   sv_setpvn(sv, "Foo", 3);
   sv_setpvn(sv, "foo", sizeof("foo")-1);
   sv_setpvn(sv, buf, buflen);
   sv_setpvn(sv, "foo", 2);
   sv_setpvn(sv, "foo", 4);
+  sv_setpvn(sv, WARNbits, WARNbits_size);
   sv_setpvs(sv, "foo");
 }
 
