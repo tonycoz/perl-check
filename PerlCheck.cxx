b0754ec58e314de496c008749ab854e86d7eb462
Tony Cook <tony@develop-help.com>
2024-10-04 21:33:12 +1000

    make the implementation more general
    I'm not entirely happy with the formatting mechanism used to build the
    replacment call, in Perl I'd use something like:
    
      join(", ", map { getargtext($_) } @args);
    
    but doing that requires a very late standard.
    
    I could use accumulate(), but I think that would require turning every
    StringRef into a std::string and more std::strings for each step of
    the accumulate, which is expensive.
    
    Of course, iostreams is expensive too.  I may re-work this soonish.

--- PerlCheck.cxx
+++ PerlCheck.cxx
@@ -8,6 +8,9 @@
 #include "clang-tidy/ClangTidyModuleRegistry.h"
 #include <string>
 #include <cassert>
+#include <sstream>
+#include <vector>
+#include <algorithm>
 
 using namespace clang;
 using namespace clang::tidy;
@@ -16,10 +19,23 @@ using namespace clang::ast_matchers;
 class PerlLiteralFunctionCheck : public ClangTidyCheck
 {
   bool UseMultiplicity;
+  std::string PvnMacro;
+  std::string PvsMacro;
+  int LiteralArgNum;
+  int LengthArgNum;
+  std::vector<int> KeepArgs;
 public:
-  PerlLiteralFunctionCheck(StringRef Name, ClangTidyContext* Context) :
+  PerlLiteralFunctionCheck(StringRef Name, ClangTidyContext* Context,
+                           StringRef PvnMacro_, StringRef PvsMacro_,
+                           int LiteralArgNum_, int LengthArgNum_,
+                           std::vector<int> &&KeepArgs_) :
     ClangTidyCheck(Name, Context),
-    UseMultiplicity(Options.getLocalOrGlobal("PerlCheckMultiplicity", 0))
+    UseMultiplicity(Options.getLocalOrGlobal("PerlCheckMultiplicity", 0)),
+    PvnMacro(PvnMacro_),
+    PvsMacro(PvsMacro_),
+    LiteralArgNum(LiteralArgNum_),
+    LengthArgNum(LengthArgNum_),
+    KeepArgs(KeepArgs_)
     {
     }
     void registerMatchers(ast_matchers::MatchFinder* Finder) override;
@@ -29,12 +45,12 @@ public:
 void PerlLiteralFunctionCheck::registerMatchers(MatchFinder* Finder)
 {
     Finder->addMatcher(
-        callExpr(isExpandedFromMacro("sv_setpvn"),
-                 hasArgument(1+UseMultiplicity,
+        callExpr(isExpandedFromMacro(PvnMacro),
+                 hasArgument(LiteralArgNum+UseMultiplicity,
                              traverse(TK_IgnoreUnlessSpelledInSource,
                                       stringLiteral().bind("literal"))
                  ),
-                 hasArgument(2+UseMultiplicity, anything())
+                 hasArgument(LengthArgNum+UseMultiplicity, anything())
         ).bind("call"), this);
 }
 
@@ -50,7 +66,7 @@ void PerlLiteralFunctionCheck::check(const MatchFinder::MatchResult& Result)
 
     const auto *Args = matchedCall->getArgs();
 
-    const auto sizeArg = Args[numArgs-1]->IgnoreUnlessSpelledInSource();
+    const auto sizeArg = Args[UseMultiplicity+LengthArgNum]->IgnoreUnlessSpelledInSource();
     Expr::EvalResult sizeIntResult;
     if (!sizeArg->EvaluateAsInt(sizeIntResult, *Result.Context)
         || !sizeIntResult.Val.isInt())
@@ -62,12 +78,24 @@ void PerlLiteralFunctionCheck::check(const MatchFinder::MatchResult& Result)
       return;
     const auto *svArg = Args[UseMultiplicity];
     const LangOptions &Opts = getLangOpts();
-    const auto svArgText = Lexer::getSourceText(CharSourceRange::getTokenRange(svArg->getSourceRange()), *Result.SourceManager, Opts);
-    const auto litText = Lexer::getSourceText(CharSourceRange::getTokenRange(strLit->getSourceRange()), *Result.SourceManager, Opts);
-    std::string repl = formatv("sv_setpvs({0}, {1})", svArgText, litText);
+    std::stringstream srepl;
+    auto argString = [this, Args, Opts, Result](auto ArgNum){
+      return Lexer::getSourceText(
+          CharSourceRange::getTokenRange(
+              Args[UseMultiplicity+ArgNum]->getSourceRange()
+          ),
+          *Result.SourceManager, Opts
+       ).operator std::string_view();
+    };
+    srepl << PvsMacro << '(' << argString(KeepArgs[0]);
+    for (auto i = std::next(KeepArgs.begin()); i != KeepArgs.end(); ++i) {
+      srepl << ", " << argString(*i);
+    }
+    srepl << ')';
+    std::string repl = srepl.str();
     auto hint = FixItHint::CreateReplacement(matchedCall->getSourceRange(), repl);
-    diag(matchedCall->getExprLoc(), "sv_setpvn() with literal better written as sv_setpvs()")
-      << hint;
+    diag(matchedCall->getExprLoc(), "%0() with literal better written as %1()")
+      << PvnMacro << PvsMacro << hint;
 }
 
 namespace {
@@ -80,7 +108,12 @@ public:
       CheckFactories.registerCheckFactory(
           "perl-literal-sv_setpvn",
           [](llvm::StringRef Name, ClangTidyContext *Context){
-            return std::make_unique<PerlLiteralFunctionCheck>(Name, Context);
+            return std::make_unique<PerlLiteralFunctionCheck>(Name, Context, "sv_setpvn", "sv_setpvs", 1, 2, std::vector{ 0, 1 });
+          });
+      CheckFactories.registerCheckFactory(
+          "perl-literal-newSVpvn",
+          [](llvm::StringRef Name, ClangTidyContext *Context){
+            return std::make_unique<PerlLiteralFunctionCheck>(Name, Context, "newSVpvn", "newSVpvs", 0, 1, std::vector{ 0 });
           });
     }
 };
--- foo.c
+++ foo.c
@@ -16,11 +16,17 @@ extern void Perl_sv_setpvn(pTHX_ SV *sv, const char *s, size_t len);
 #define sv_setpvn(sv, s, len) Perl_sv_setpvn(aTHX_ sv, s, len)
 #define sv_setpvs(sv, s) Perl_sv_setpvn(aTHX_ sv, s "", sizeof(s "")-1)
 
+extern SV *Perl_newSVpvn(pTHX_ const char *s, size_t len);
+#define newSVpvn(s, len) Perl_newSVpvn(aTHX_ s, len)
+#define newSVpvs(s) Perl_newSVpvn(s, sizeof(s "")-1)
+
 char buf[10];
 int buflen;
 
 #define WARNbits "1234"
 #define WARNbits_size 4
+
+extern SV *my_sv;
 
 void foo(pTHX_ SV *sv) {
   sv_setpvn(sv, "Foo", 3);
@@ -30,6 +36,8 @@ void foo(pTHX_ SV *sv) {
   sv_setpvn(sv, "foo", 4);
   sv_setpvn(sv, WARNbits, WARNbits_size);
   sv_setpvs(sv, "foo");
+
+  my_sv = newSVpvn("ab", 2);
 }
 
 void awesome_foo() { }
